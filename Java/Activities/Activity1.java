b5b65953d51872765d8d438e90d3599e110631c3
YashwanthSubbisetty2402 <Subbisetty.VenkataSai.Yashwanth@ibm.com>
2024-06-27 11:16:34 +0530

    Add files via upload

---
+++ Java/Activities/Activity1.java
@@ -0,0 +1,22 @@
+package activities;
+
+public class Activity1 {
+
+	public static void main(String[] args) {
+
+		Car Audi = new Car();
+
+		Audi.sColor = "Red";
+		Audi.sTransmission = "Automatic";
+		Audi.iMake = 2020;
+		Audi.iTyres = 4;
+		Audi.iDoors = 4;
+
+		Audi.displayCharactristics();
+		Audi.accelarate();
+		Audi.brake();
+		
+
+	}
+
+}
