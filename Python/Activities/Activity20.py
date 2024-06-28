d029b33361f03985a8e3c24ff54f562075b8378b
YashwanthSubbisetty2402 <Subbisetty.VenkataSai.Yashwanth@ibm.com>
2024-06-28 10:58:51 +0530

    Add files via upload

---
+++ Python/Activities/Activity20.py
@@ -0,0 +1,22 @@
+# Import pandas
+import pandas
+
+# Read data from Excel sheet
+dataframe = pandas.read_excel('sample.xlsx')
+
+
+print(dataframe)
+
+
+print("====================================")
+print("Number of rows and columns:", dataframe.shape)
+
+
+print("====================================")
+print("Emails:")
+print(dataframe['Email'])
+
+
+print("====================================")
+print("Sorted data:")
+print(dataframe.sort_values('FirstName'))
